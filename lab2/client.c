d4327907bb5534b536231bf53f07a6119ebc3e10
Richard Zhou <zhourichard19@gmail.com>
2022-05-06 13:53:16 -0400

    lab2

--- lab2/client.c
+++ lab2/client.c
@@ -8,7 +8,6 @@
 #include <unistd.h>
 #include <ctype.h>
 
-#define WINDOWSIZE 20
 #define MAXWAITTIME 1
 #define SENDSIZE 3
 
@@ -16,9 +15,9 @@ int sendStuff(char *buffer, int sd, struct sockaddr_in server_address);
 char *rtrim(char *s);
 int main(int argc, char *argv[])
 {
-  int sd;
+  int sd;//creates needed variables
   struct sockaddr_in server_address;
-  char buffer[100] = "I would like green eggs and ham. What would you like to eat today?";
+  char buffer[100] = "1I would like green eggs and ham. What would you like to eat today?";
   
   int portNumber;
   char serverIP[29];
@@ -37,7 +36,7 @@ int main(int argc, char *argv[])
   portNumber = strtol(argv[2], NULL, 10);
   strcpy(serverIP, argv[1]);
   
-  server_address.sin_family = AF_INET;
+  server_address.sin_family = AF_INET;//creates server ports and sockets
   server_address.sin_port = htons(portNumber);
   server_address.sin_addr.s_addr = inet_addr(serverIP);
   sendStuff(buffer, sd, server_address);
@@ -53,7 +52,6 @@ int sendStuff(char *buffer, int sd, struct sockaddr_in server_address){
   time_t currentTime;
   int currentFrame;
   int rc;
-  int maxWindow = WINDOWSIZE;
   char bufferOut[100];
   struct sockaddr_in fromAddress;
   socklen_t fromLength = sizeof(struct sockaddr_in);
@@ -65,6 +63,7 @@ int sendStuff(char *buffer, int sd, struct sockaddr_in server_address){
   int sentWindowSize = 0;
   int dataSize = sizeOfSendBuffer;
   int basePacketSize = 15;
+  int resendFrame=0;
   
   totalBytesToSend = strlen(buffer); //change this once we add in file IO
   printf ("totalBytesToSend %d\n", totalBytesToSend);
@@ -77,62 +76,97 @@ int sendStuff(char *buffer, int sd, struct sockaddr_in server_address){
   totalBytesToSend = ntohl(totalBytesToSend);
   currentFrame = 0;
   sizeOfSendBuffer = 2; // will change potentially for last frame
-  sprintf (bufferOut, "%11d%4d", (currentFrame),(sizeOfSendBuffer));
-  
-  if (bytesLeftToSend - sizeOfSendBuffer <0){
-    strncpy(&bufferOut[15],&buffer[currentFrame], (bytesLeftToSend));
-    dataSize = bytesLeftToSend;
-    bytesLeftToSend = 0;
-  }
-  else{
-    strncpy(&bufferOut[15],&buffer[currentFrame], sizeOfSendBuffer); //Data
-    bytesLeftToSend -= sizeOfSendBuffer;
-    dataSize = sizeOfSendBuffer;
-      
-    printf("sending '%s'\n", bufferOut);       
-    rc = sendto(sd, bufferOut, basePacketSize+dataSize, 0,(struct sockaddr *) &server_address, sizeof(server_address));   
-    if (rc < 0)
+  int a = 0;
+  int done = 0;
+
+  while(a < totalBytesToSend){ //maks sure that while there is data to send the client will keep sending data
+    memset(bufferOut, 0, 100); // sets thhe memory fo the bufferr
+    if(bytesLeftToSend == 1){  //checks to make sure if there are 
+      sizeOfSendBuffer = 1;
+      done = 1;
+    }
+    sprintf (bufferOut, "%11d%4d", (currentFrame),(sizeOfSendBuffer)); // prints out what you are sending
+
+    if (bytesLeftToSend - sizeOfSendBuffer < 0){ //when there are no more things to send, exit the function
+      strncpy(&bufferOut[15],&buffer[currentFrame], (bytesLeftToSend));  //loads the buffer with what is going to be needed to send
+      dataSize = bytesLeftToSend;  //updates the amount of data needed to be sent
+      bytesLeftToSend = 0;
+      printf("NO MORE BYTES LEFT TO SEND\n");
+      done = 1;
+    }
+    else{
+      strncpy(&bufferOut[15],&buffer[currentFrame], sizeOfSendBuffer); //loads the data from the buffer to be sent
+      bytesLeftToSend -= sizeOfSendBuffer;  //
+      dataSize = sizeOfSendBuffer;
+    }
+    printf("sending '%s'\n", bufferOut);       //shows what the buffer is
+    rc = sendto(sd, bufferOut, basePacketSize+dataSize, 0,(struct sockaddr *) &server_address, sizeof(server_address)); //sends message to the server
+    
+    if (rc < 0) // checks for the rc and if it is less tan 0 throws an error
       perror("sendto");
     printf("sent %d bytes, seqNumber %d, bottomOfWindow %d\n", rc, currentFrame, bottomOfWindow);
-    if(currentFrame == bottomOfWindow){
+    if(currentFrame == bottomOfWindow){// checks to see if the message is in the right place and gives a sleep timerr
       timeSent = time(NULL);
+      sleep(2);
     }
-    currentFrame += dataSize;
-    sentWindowSize += dataSize;
-}// end of the if there is room to sne dstuff statment
+    currentFrame += dataSize; //move the current frame
+    sentWindowSize += dataSize;// changes the window size to shift
 
-  memset(bufferRead, 0, 100);
-  rc = recvfrom(sd, &bufferRead, 100, MSG_DONTWAIT, (struct sockaddr *)&fromAddress, &fromLength);
+    memset(bufferRead, 0, 100);
+    rc = recvfrom(sd, &bufferRead, 100, MSG_DONTWAIT, (struct sockaddr *)&fromAddress, &fromLength); //lloks for the rc code of reception
   
-  if(rc > 0){
-    sscanf(bufferRead, "%11d", &ackNumber);
-    printf ("ACK received %d \n",ackNumber);
-    printf ("received data %d bytes, ack is %d, bottom/top Window %d/%d\n",rc, ackNumber, bottomOfWindow, topOfWindow);
+    if(rc > 0){ //when the rc code is given updates information
+      sscanf(bufferRead, "%11d", &ackNumber); //gets the ACK number
+      printf ("ACK received %d \n",ackNumber);
+      printf ("received data %d bytes, ack is %d, bottom/top Window %d/%d\n",rc, ackNumber, bottomOfWindow, topOfWindow);
+      resendFrame = currentFrame; //keeps track of when the ACK has been received
+      printf("Updated the resend Frame to %d. Current Frame is %d\n",resendFrame,currentFrame);
 
-    if(ackNumber >= bottomOfWindow){
-      bottomOfWindow = ackNumber + dataSize;
-      sentWindowSize = currentFrame - bottomOfWindow;
-      topOfWindow = bottomOfWindow + sentWindowSize;
-      timeSent = time(NULL);
-    }  
-  }
-  currentTime = time(NULL);
-  if (currentTime - timeSent > MAXWAITTIME){
-    printf("\n\nTIMEOUT ****** should do a resend\n\n\n");
-    int i;
-    int topOfWindow = bottomOfWindow + sentWindowSize;
-    printf("top of window %d, bottom %d, sentsize %d\n", topOfWindow, bottomOfWindow, sentWindowSize);
-    for (i = bottomOfWindow; i < topOfWindow;){
-      memset (bufferOut, 0, 100);
+      if(ackNumber >= bottomOfWindow){  // Shifts the window to accomadate for the ACK
+        bottomOfWindow = ackNumber + dataSize;
+        sentWindowSize = currentFrame - bottomOfWindow;
+        topOfWindow = bottomOfWindow + sentWindowSize;
+        timeSent = time(NULL);
+      }  
+    }
+    currentTime = time(NULL); //sets the time that the message was just sent
+    
+    if (currentTime - timeSent > MAXWAITTIME){ //when there is no ACK that returns, throws a timeout
+      printf("\n\nTIMEOUT ****** should do a resend\n\n\n");
+      int i;
+      int topOfWindow = bottomOfWindow + sentWindowSize;
+      printf("top of window %d, bottom %d, sentsize %d\n", topOfWindow, bottomOfWindow, sentWindowSize);
+      for (i = bottomOfWindow; i < topOfWindow;){ //loops to resend the packets from when the ACK was not received.
+        printf("ACK %d, bottomWindow %d\n",ackNumber, bottomOfWindow);
+        memset (bufferOut, 0, 100);
       
-      char *ptr;
-      ptr = &buffer[i];
-      printf ("str is '%s', length is %lu\n",ptr, strlen(ptr));
+        char *ptr;
+        ptr = &buffer[i];
+        printf ("str is '%s', length is %lu\n",ptr, strlen(ptr));
 
-      if(strlen(ptr) ==1){
-        sprintf(bufferOut, "%11d%4d", (i),1);
+        if(bytesLeftToSend ==1){
+          sprintf(bufferOut, "%11d%4d", (i),1);
+        }
+	else{
+	  sprintf (bufferOut, "%11d%4d", (resendFrame),(sizeOfSendBuffer));//loads the data into buffer
+	}
+	
+        strncpy(&bufferOut[15],&buffer[resendFrame], sizeOfSendBuffer); //Data
+        dataSize = sizeOfSendBuffer;
+	printf("resending '%s'\n", bufferOut);
+      
+        
+        rc = sendto(sd, bufferOut, basePacketSize+dataSize, 0,(struct sockaddr *) &server_address, sizeof(server_address)); 
+        
+        i += 2;
       }
-    } 
+      bottomOfWindow = currentFrame;//resets the bottom of window to match
+      resendFrame = currentFrame;
+    }
+    if (done == 1){
+      a = totalBytesToSend + 1;
+    }
+    a = a + 1;
   }
   return 0;
 }
